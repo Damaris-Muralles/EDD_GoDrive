f675ecefc0f3a5c9883ba4f985889b37b88d7eb1
DM <damarisyulizzamuralles@gmail.com>
2023-03-20 17:22:49 -0600

    Tercer commit: pagina principal y login

---
+++ EDD_Proyecto1_Fase2/JS/index_dinamic.js
@@ -0,0 +1,17 @@
+/*
+    Autor: DM
+    Año:2023
+*/
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle('header2',window.scrollY>0);
+   
+    var logo =document.querySelector(".logo img");
+    if (window.scrollY>0){
+        logo.setAttribute('src','../Img/logo.png');
+        logo.setAttribute('width','250');
+    }else{
+        logo.setAttribute('src','../Img/vacio.png');
+        logo.setAttribute('width','10');
+    }
+});
