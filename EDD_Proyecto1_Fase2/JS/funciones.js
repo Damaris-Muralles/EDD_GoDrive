e0125d48eee39a3f99294d2a48918da769e6ba9a
DM <damarisyulizzamuralles@gmail.com>
2023-03-27 16:50:06 -0600

    Septimo commit: cambios en tablero e implementacion de algunas funciones en js

---
+++ EDD_Proyecto1_Fase2/JS/funciones.js
@@ -0,0 +1,47 @@
+// REDIRECCIONAMIENTOS
+function Rprincipal(){
+    location.href="../Pag_inicio/principal.html";
+}
+function Rlogin(){
+    location.href="../Pag_inicio/Login.html";
+}
+function Radmin(){
+    location.href = '../Pag_admin/tablero_admin.html';
+}
+function Restudent(){
+    location.href = '../Pag_user/tablero_student.html';
+}
+
+
+//FUNCIONES PARA INICIO DE SESION
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var data = new FormData(form);
+    var username = data.get('username');
+    var password = data.get('passworduser');
+    if (username=="admin"){
+        if(password=="admin"){
+            Radmin();
+        }else{
+            Swal.fire(
+                'Contraseña incorrecta',
+                'Haga click en el boton para continuar...',
+                'error'
+              )
+        }
+    }else{
+        Restudent();
+    }
+});
+//FUNCIONES PARA CERRAR SESION
+function cerrarsesion(){
+    Rlogin();
+}
+
+
+//FUNCIONES DEL ADMINISTRADOR
+//-> 1. carga de archivos
+
+//FUNCIONES DE LOS ESTUDIANTIES
